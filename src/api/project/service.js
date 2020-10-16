15f01f22e9d264eb1063cc2d10dfaeedfeb53dcd
wuth <lonelywth@163.com>
2020-10-16 16:21:10 +0800

    fix(gaad)： 页面功能开发
    1.完成服务页面接口对接
    2.调整logo显示和页面文案展示

---
+++ src/api/project/service.js
@@ -0,0 +1,60 @@
+import request from "@/router/axios";
+import {baseUrl} from "@/config/env";
+
+/**
+ * 新增服务
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export const createService = (params) => request({
+    url: baseUrl + '/service/createService',
+    method: 'post',
+    data: params
+})
+/**
+ * 分页查询服务
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export const pageService = (params) => request({
+    url: baseUrl + '/service/pageServices',
+    method: 'post',
+    data: params
+})
+/**
+ * 更新服务
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export const updateService = (params) => request({
+    url: baseUrl + '/service/updateService',
+    method: 'put',
+    data: params
+})
+
+
+/**
+ * 删除服务
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export const deleteService = (params) => request({
+    url: baseUrl + '/service/deleteService',
+    method: 'delete',
+    data: params
+})
+/**
+ * 构建项目
+ * @param params
+ * @returns {AxiosPromise}
+ */
+export const deployService = (params) => request({
+    url: baseUrl + '/service/deploy',
+    method: 'post',
+    data: params
+})
+
+
+
+
+
