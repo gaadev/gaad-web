4e9ee0146850f7844a83b4339fea5d9b2e94f510
wuth <lonelywth@163.com>
2020-10-16 16:21:27 +0800

    fix(gaad)： 页面功能开发
    1.完成服务页面接口对接
    2.调整logo显示和页面文案展示

---
+++ src/views/question/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <basic-container>
+    <p v-for="(item,index) in list" :key="index">{{ item }}</p>
+  </basic-container>
+</template>
+<script>
+
+export default {
+  name: 'question',
+  data() {
+    return {
+      list: [{
+        question: "新增服务时，指定端口，进行服务端口映射，默认80,则不生成template，如果改变了，则生成新的配置文件",
+        date: '2020-10-12',
+        status: '未解决'
+      },{
+        question: "k8s初始化脚本，未yum update",
+        date: '2020-10-15  解决时间：2020-10-16',
+        status: '已解决'
+      },{
+        question: "批量构建和批量添加如何操作",
+        date: '2020-10-16',
+        status: '未解决'
+      }]
+    }
+  },
+  created() {
+
+  },
+  methods: {},
+}
+</script>
